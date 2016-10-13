717844c68604be6671b675bf7dd15809784d3d47
blongden73 <hello@benlongdendesign.co.uk>
2016-10-13 09:34:16 +0100

    some chnages

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ import share from './lib/share'
 var shareFn = share('Interactive title', 'http://gu.com/p/URL', '#Interactive');
 var el;
 var data;
+var comingSoonIndex = 0;
 
 export function init(dom, context, config, mediator) {
 
@@ -19,9 +20,29 @@ export function init(dom, context, config, mediator) {
           data = resp;
           // console.log('hello json');
           // console.log(data);
+
+          for(var i = 0; i < data.sheets.Sheet1.length; i++) {
+              if(data.sheets.Sheet1[i].Description === ""){
+                comingSoonIndex = i - 1;
+                i = data.sheets.Sheet1.length + 1;
+              }
+          }
+
+          function returnPart(){
+            for(var i = 0; i < data.sheets.Sheet1.length; i++) {
+                if (window.location.href === data.sheets.Sheet1[i].Link) {
+                    var part = i + 1;
+                    return part;
+                }
+            }
+          }
+
+          var date = data.sheets.Sheet1[comingSoonIndex].Date;
+          var comingSoon = "<h5>Coming on " + date + "</h5>" + data.sheets.Sheet1[comingSoonIndex].Description;
+          initPageElements(comingSoon, returnPart());
+          console.log('parting 5');
       }
   });
-
 
   // Check if this is the callout page
   if (window.location.href.indexOf('us-election-gary-younge-muncie-indiana')>0) {
@@ -30,11 +51,11 @@ export function init(dom, context, config, mediator) {
     var isCalloutPage = false;
   }
 
+
   // timeout prevents a flash of unstyled elements
-  setTimeout(function() {
+  function initPageElements(comingSoonText, returnPart){
     var textBody = document.querySelector('.content__article-body');
     var headlineContainer = document.querySelector('.explore-series-headline .content__main-column');
-
 
     var atomItem = document.createElement("div");
     atomItem.classList.add('sidebar-atom');
@@ -48,7 +69,7 @@ export function init(dom, context, config, mediator) {
 
     var seriesPageItem = document.createElement("div");
     seriesPageItem.classList.add('series-page-link');
-    seriesPageItem.innerHTML = "<div class='part'><strong>Part 1</strong> of a 9 part series</div><div class='why' data-link-name='middletown : why are we covering this'>Why are we covering this?</div><a class='get-involved' href='https://www.theguardian.com/membership/2016/oct/11/us-election-gary-younge-muncie-indiana' data-link-name='middletown : get involved with this project'>Get involved with this project</a>";
+    seriesPageItem.innerHTML = "<div class='part'><strong>Part " + returnPart + "</strong> of a 9 part series</div><div class='why' data-link-name='middletown : why are we covering this'>Why are we covering this?</div><a class='get-involved' href='https://www.theguardian.com/membership/2016/oct/11/us-election-gary-younge-muncie-indiana' data-link-name='middletown : get involved with this project'>Get involved with this project</a>";
 
     var overlayItem = document.createElement("div");
     overlayItem.classList.add('sidebar-overlay');
@@ -57,19 +78,17 @@ export function init(dom, context, config, mediator) {
     // to-do update with spreadsheet text
     var newsletterItem = document.createElement("div");
     newsletterItem.classList.add('boot_newsletter');
-    newsletterItem.innerHTML = "<h5>Coming on Thursday 13 October</h5><p>I'll share my first impressions of Muncie, and the political and societal divides in the town, having arrived as the second presidential debate aired. Do get in touch with your views.</p><form method='post' novalidate action='https://guardiannewsampampmedia.formstack.com/forms/index.php' class='fsForm fsSingleColumn fsMaxCol1' id='fsForm2490271'><input type='hidden' name='form' value='2490271' /><input type='hidden' name='viewkey' value='tOfqZ3Kxj2' /><input type='hidden' name='password' value='' /><input type='hidden' name='hidden_fields' id='hidden_fields2490271' value='' /><input type='hidden' name='fspublicsession' id='session_id2490271' value='' /><input type='hidden' name='incomplete' id='incomplete2490271' value='' /><input type='hidden' name='incomplete_email' id='incomplete_email2490271' value='' /><input type='hidden' name='incomplete_password' id='incomplete_password2490271' /><input type='hidden' name='referrer' id='referrer2490271' value='' /><input type='hidden' name='referrer_type' id='referrer_type2490271' value='link' /><input type='hidden' name='_submit' value='1' /><input type='hidden' name='style_version' value='3' /><input type='hidden' id='fsLatitude' name='latitude' value='' /><input type='hidden' id='fsLongitude' name='longitude' value='' /><input type='hidden' id='viewparam' name='viewparam' value='551498' /><input type='hidden' id='analytics' name='analytics' value='' /><input type='hidden' id='fsSaveResumePassword2490271' value='1' /><label id='label46135110' class='fsLabel fsRequiredLabel' for='field46135110'>Sign up for an email alert when new articles are published</label><input type='email' placeholder='Your email' id='field46135110' name='field46135110' required='required' value='' class='fsField fsFormatEmail fsRequired' aria-required='true' /><div class='marketing'><input type='checkbox' id='field46329861_1' name='field46329861[]' value='I'd also like to receive the latest updates and offers from the Guardian' checked='checked' class='fsField vertical' data-link-name='middletown : newsletter checkbox'><label class='fsOptionLabel vertical' for='field46329861_1'  data-link-name='middletown : newsletter checkbox text'>I'd also like to receive the latest updates and offers from the Guardian</label><p>By proceeding, you agree to the Guardian's <a href='https://www.theguardian.com/help/terms-of-service'>Terms of Service</a> &amp; <a href='https://www.theguardian.com/info/privacy'>Privacy Policy</a>.</p></div><input id='fsSubmitButton2490271' class='fsSubmitButton' type='submit' value='Subscribe' data-link-name='middletown : newsletter sign up in-article' /></form>";
+    newsletterItem.innerHTML = comingSoonText + "<form method='post' novalidate action='https://guardiannewsampampmedia.formstack.com/forms/index.php' class='fsForm fsSingleColumn fsMaxCol1' id='fsForm2490271'><input type='hidden' name='form' value='2490271' /><input type='hidden' name='viewkey' value='tOfqZ3Kxj2' /><input type='hidden' name='password' value='' /><input type='hidden' name='hidden_fields' id='hidden_fields2490271' value='' /><input type='hidden' name='fspublicsession' id='session_id2490271' value='' /><input type='hidden' name='incomplete' id='incomplete2490271' value='' /><input type='hidden' name='incomplete_email' id='incomplete_email2490271' value='' /><input type='hidden' name='incomplete_password' id='incomplete_password2490271' /><input type='hidden' name='referrer' id='referrer2490271' value='' /><input type='hidden' name='referrer_type' id='referrer_type2490271' value='link' /><input type='hidden' name='_submit' value='1' /><input type='hidden' name='style_version' value='3' /><input type='hidden' id='fsLatitude' name='latitude' value='' /><input type='hidden' id='fsLongitude' name='longitude' value='' /><input type='hidden' id='viewparam' name='viewparam' value='551498' /><input type='hidden' id='analytics' name='analytics' value='' /><input type='hidden' id='fsSaveResumePassword2490271' value='1' /><label id='label46135110' class='fsLabel fsRequiredLabel' for='field46135110'>Sign up for an email alert when new articles are published</label><input type='email' placeholder='Your email' id='field46135110' name='field46135110' required='required' value='' class='fsField fsFormatEmail fsRequired' aria-required='true' /><div class='marketing'><input type='checkbox' id='field46329861_1' name='field46329861[]' value='I'd also like to receive the latest updates and offers from the Guardian' checked='checked' class='fsField vertical' data-link-name='middletown : newsletter checkbox'><label class='fsOptionLabel vertical' for='field46329861_1'  data-link-name='middletown : newsletter checkbox text'>I'd also like to receive the latest updates and offers from the Guardian</label><p>By proceeding, you agree to the Guardian's <a href='https://www.theguardian.com/help/terms-of-service'>Terms of Service</a> &amp; <a href='https://www.theguardian.com/info/privacy'>Privacy Policy</a>.</p></div><input id='fsSubmitButton2490271' class='fsSubmitButton' type='submit' value='Subscribe' data-link-name='middletown : newsletter sign up in-article' /></form>";
 
+      if (!isCalloutPage) {
+        textBody.insertBefore(atomItem, textBody.childNodes[4]);
+        textBody.insertBefore(newsletterItem, textBody.childNodes[textBody.childNodes.length]);
+        headlineContainer.appendChild(seriesPageItem);
+        textBody.insertBefore(overlayItem, textBody.childNodes[0]);
+        textBody.insertBefore(linkItem, textBody.childNodes[20]);
 
-    if (!isCalloutPage) {
-      textBody.insertBefore(atomItem, textBody.childNodes[4]);
-      textBody.insertBefore(newsletterItem, textBody.childNodes[textBody.childNodes.length]);
-      headlineContainer.appendChild(seriesPageItem);
-      textBody.insertBefore(overlayItem, textBody.childNodes[0]);
-      textBody.insertBefore(linkItem, textBody.childNodes[20]);
-
-      document.querySelector('.content__standfirst--explore').classList.add('quote');
-    }
-
+        document.querySelector('.content__standfirst--explore').classList.add('quote');
+      }
 
     [].forEach.call(document.querySelectorAll('.content__series-label__link'), function(link) {
       link.setAttribute('href', 'https://www.theguardian.com/membership/ng-interactive/2016/oct/11/view-middletown-gary-younge-us-presidential-election');
@@ -124,7 +143,7 @@ export function init(dom, context, config, mediator) {
 
     // delete until here --
 
-  }, 100);
+  };
 
 
   function is_touch_device() {
