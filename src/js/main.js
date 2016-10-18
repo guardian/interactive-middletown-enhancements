3f26a80c21fa2ddbeb67e5a5562e37fc681f0dde
Duarte Carrilho da Graça <duarte.carrilho.freelance@theguardian.com>
2016-10-18 14:24:47 +0100

    bugfix

--- src/js/main.js
+++ src/js/main.js
@@ -27,9 +27,8 @@ export function init(dom, context, config, mediator) {
           }
 
           function returnPart(){
-            var link = data.sheets.Sheet1[i].Link.split('/');
-            console.log(link[3]);
             for(var i = 0; i < data.sheets.Sheet1.length; i++) {
+              var link = data.sheets.Sheet1[i].Link.split('/');
                 if (window.location.href === data.sheets.Sheet1[i].Link) {
                     var part = i + 1;
                     return part;
